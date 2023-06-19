e821acfcceaa7065adf4ffc4e9158e1a42d8cd8b
Lukas Boehler <lukas@boehlerbrothers.com>
2023-06-19 17:13:15 +0200

    Init.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import Gleap from 'gleap'
+
+defineProps<{
+  msg: string
+}>()
+
+function greet() {
+  Gleap.open();
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a>.
+    </h3>
+    <button @click="greet">Greet</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import Gleap from 'gleap';
+import App from './App.vue';
+
+Gleap.initialize("0Iszo57uOPq1KiFu4FIaljKHbXWFwLW4");
+
+createApp(App).mount('#app')
